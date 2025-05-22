72f7b9a040da9833ef0fdeb88a6c5101c8da19fa
RoHaNRaJ <reaPeR10x10x@gmail.com>
2025-05-22 09:20:03 +0530

    writeback: apply init protection to some nodes Instead of hardcoding them let's block init changes using task_is_booster
    Signed-off-by: RoHaNRaJ <reaPeR10x10x@gmail.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -39,6 +39,7 @@
 #include <linux/sched/signal.h>
 #include <linux/mm_inline.h>
 #include <trace/events/writeback.h>
+#include <linux/binfmts.h>
 
 #include "internal.h"
 
@@ -71,7 +72,7 @@ static long ratelimit_pages = 32;
 /*
  * Start background writeback (via writeback threads) at this percentage
  */
-int dirty_background_ratio = 10;
+int dirty_background_ratio = 5;
 
 /*
  * dirty_background_bytes starts at 0 (disabled) so that it is a function of
@@ -528,6 +529,9 @@ int dirty_background_ratio_handler(struct ctl_table *table, int write,
 {
 	int ret;
 
+        if (task_is_booster(current))
+ 		return 0;
+
 	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
 	if (ret == 0 && write)
 		dirty_background_bytes = 0;
@@ -540,6 +544,9 @@ int dirty_background_bytes_handler(struct ctl_table *table, int write,
 {
 	int ret;
 	unsigned long old_bytes = dirty_background_bytes;
+
+        if (task_is_booster(current))
+ 		return 0;
 
 	ret = proc_doulongvec_minmax(table, write, buffer, lenp, ppos);
 	if (ret == 0 && write) {
@@ -560,6 +567,9 @@ int dirty_ratio_handler(struct ctl_table *table, int write,
 	int old_ratio = vm_dirty_ratio;
 	int ret;
 
+        if (task_is_booster(current))
+ 		return 0;
+
 	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
 	if (ret == 0 && write && vm_dirty_ratio != old_ratio) {
 		writeback_set_ratelimit();
@@ -574,6 +584,9 @@ int dirty_bytes_handler(struct ctl_table *table, int write,
 {
 	unsigned long old_bytes = vm_dirty_bytes;
 	int ret;
+
+        if (task_is_booster(current))
+ 		return 0;
 
 	ret = proc_doulongvec_minmax(table, write, buffer, lenp, ppos);
 	if (ret == 0 && write && vm_dirty_bytes != old_bytes) {
